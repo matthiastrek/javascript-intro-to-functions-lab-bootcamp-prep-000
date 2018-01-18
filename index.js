53205c80f519575c191c447bc440db697a4b77f6
Matthew Trenk <matt.trenk@gmail.com>
2018-01-18 02:22:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var upperCase = 'I can\'t hear you!'
-  upperCase.toUpperCase() === upperCase
-  return upperCase
+  if (string.toUpperCase() === false) {
+    console.log('I can\'t hear you!')
+  } else if (string.toUpperCase() === true) {
 }
 
 function sayHiToGrandma(string) {
